e3a14d5d9b56d3ea98f79f7c84a2461b1f4b41a9
Robert Nolan <robert@vohgo.com>
2016-04-15 20:25:47 -0500

    Added chrome build files and manifest

---
+++ main.js
@@ -0,0 +1,304 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	const counter = __webpack_require__(1)
+	const storage = __webpack_require__(2)
+	const timerElem = __webpack_require__(3)
+	const video = __webpack_require__(5)
+
+	const TODAY = new Date()
+	const DEFAULT_TIME_LEFT = 3600; // In seconds
+	const STORAGE_KEY = `timeLeft.${TODAY.getYear()}-${TODAY.getMonth()}-${TODAY.getDate()}`
+
+	function initTimer (time) {
+	  timerElem.label(time)
+
+	  counter
+	    .setTime(time)
+	    .each((tl) => {
+	      storage.saveTime(tl)
+	      timerElem.label(tl)
+	    })
+	    .done(() => {
+	      video.pause()
+	      timerElem.done()
+	    })
+	}
+
+	function initVideoPage() {
+	  var location = window.location.pathname + '';
+
+	  if (/watch/.test(location)) {
+	    video.init()
+	    video.onPause(counter.pause)
+	    video.onPlay(counter.play)
+
+	    counter.play()
+	  } else {
+	    counter.pause()
+	  }
+	}
+
+	/**
+	 * Let's get this party started
+	 */
+
+	storage.init(STORAGE_KEY, DEFAULT_TIME_LEFT)
+	storage.getTime().then(initTimer)
+
+	initVideoPage();
+
+	/**
+	 * Page page listener
+	 */
+	(document.body || document.documentElement)
+	  .addEventListener('transitionend', event => {
+	    if (event.propertyName === 'width' && event.target.id === 'progress') {
+	      initVideoPage()
+	    }
+	  }, true);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	const INTERVAL_DELAY = 1000
+
+	let doneCallback = () => {}
+	let eachCallback = () => {}
+
+	let interval
+	let timeLeft = 0
+
+	let _return = {}
+
+	function _countDown () {
+	  let nextTime = timeLeft - 1
+
+	  eachCallback(nextTime)
+
+	  if (nextTime === 0) {
+	    doneCallback()
+	    pause()
+	  }
+
+	  timeLeft = nextTime
+	}
+
+	function each (c) {
+	  eachCallback = c
+	  return _return
+	}
+
+	function done (c) {
+	  doneCallback = c
+	  return _return
+	}
+
+	function pause () {
+	  clearInterval(interval)
+	  return _return
+	}
+
+	function play () {
+	  clearInterval(interval)
+	  interval = setInterval(_countDown, INTERVAL_DELAY)
+	  return _return
+	}
+
+	function setTime (newTime) {
+	  timeLeft = newTime
+	  return _return
+	}
+
+	module.exports = _return = {
+	  done,
+	  each,
+	  pause,
+	  play,
+	  setTime
+	}
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	let storageKey = 'key'
+	let defaultTime = 0
+
+	exports.init = function (k, t) {
+	  storageKey = k
+	  defaultTime = t
+	}
+
+	exports.getTime = function () {
+	  return new Promise((resolve) => {
+	    chrome.storage.local.get(storageKey, (result) => {
+	      const value = result[storageKey];
+	      resolve(typeof value !== 'undefined' ? value : defaultTime)
+	    })
+	  })
+	}
+
+	exports.saveTime = function (t) {
+	  return new Promise((resolve) => {
+	    let storageObj = {}
+	    storageObj[storageKey] = t
+	    chrome.storage.local.set(storageObj, (result) => {
+	      resolve(result)
+	    })
+	  })
+	}
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	const humanizeSeconds = __webpack_require__(4)
+
+	let widget = document.createElement('div')
+	let label = document.createElement('span')
+
+	/**
+	 * Initialize
+	 */
+
+	label.classList.add('youtube-blocker-label')
+	label.innerText = '....'
+
+	widget.classList.add('youtube-blocker-widget')
+	widget.appendChild(label)
+
+	document.body.appendChild(widget)
+
+	/**
+	 * Public methods
+	 */
+
+	exports.label = function (time) {
+	  label.innerText = humanizeSeconds(time)
+	}
+
+	exports.done = function () {
+	  widget.classList.add('youtube-blocker-widget-done')
+	}
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	module.exports = function humanizeSeconds (totalSeconds) {
+	  let minutes = Math.floor(totalSeconds / 60)
+	  let seconds = totalSeconds - (minutes * 60);
+	  return minutes + ':' + (seconds > 9 ? seconds : '0' + seconds);
+	}
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	'use strict';
+	let video
+
+	let onPauseCallback = () => {}
+	let onPlayCallback = () => {}
+
+	exports.init = function () {
+	  video = document.getElementsByTagName('video')[0]
+
+	  if (video) {
+	    video.addEventListener('pause', () => {
+	      onPauseCallback()
+	    })
+
+	    video.addEventListener('play', () => {
+	      onPlayCallback()
+	    })
+	  }
+	}
+
+	/**
+	 * Dispatchers
+	 */
+
+	exports.onPause = function (callback) {
+	  onPauseCallback = callback
+	}
+
+	exports.onPlay = function (callback) {
+	  onPlayCallback = callback
+	}
+
+
+	/**
+	 * Listeners
+	 */
+
+	exports.play = function () {
+	  video.play()
+	}
+
+	exports.pause = function () {
+	  video.pause()
+	}
+
+
+/***/ }
+/******/ ]);
